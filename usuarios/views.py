fd7d40290ead1b3c3c6fdd0d0927d94e3c042451
Diego Faria <diegoafbrito12@gmail.com>
2023-04-19 21:21:54 -0400

    añadir boton de salir

--- usuarios/views.py
+++ usuarios/views.py
@@ -6,7 +6,14 @@ from django.http.response import HttpResponse
 # Create your views here.
 
 def bienvenida(request):
-    return HttpResponse("¡Usted ha iniciado sesión!")
+    if request.method == 'POST':
+        return redirect('/usuarios/login/') 
+    print(request.user)
+    if request.user.is_authenticated:
+        return render(request, 'registration/bienvenida.html', {})
+    
+    print("Patata")
+    return redirect('/usuarios/login/')
 
 def register_user(request):
     return render(request, 'registration/register.html', {})
