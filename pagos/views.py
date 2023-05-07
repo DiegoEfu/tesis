3fa6b5ff5a7899fc6758a0b8ea89de7a185dfd49
Diego Faria <diegoafbrito12@gmail.com>
2023-05-07 16:27:55 -0400

    crear vistas de los formularios

--- pagos/views.py
+++ pagos/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def formulario_pago(request):
+    # Vista del formulario de registro de pago
+
+    # Redirigir si no está autenticado
+    # ! Una vez esté listo el formulario hay que eliminar esta y descomentar las siguientes dos líneas:
+    # if(not request.user.is_authenticated):
+    #     return redirect('/usuarios/login/')
+
+    if(request.method == 'GET'):
+        # Construcción del formulario a partir de la plantilla
+        return render(request, "formulario_pago.html")
+    elif(request.method == 'POST'):
+        # Validación de completitud de datos
+        # Validación de correcta estructura de datos
+        # Creación
+        pass
+
+def formulario_cuenta(request):
+    # Vista del formulario de registro de pago
+
+    # Redirigir si no está autenticado
+    # ! Una vez esté listo el formulario hay que eliminar esta y descomentar las siguientes dos líneas:
+    # if(not request.user.is_authenticated):
+    #     return redirect('/usuarios/login/')
+
+    if(request.method == 'GET'):
+        # Construcción del formulario a partir de la plantilla
+        return render(request, "formulario_cuenta.html")
+    elif(request.method == 'POST'):
+        # Validación de completitud de datos
+        # Validación de correcta estructura de datos
+        # Creación
+        pass
