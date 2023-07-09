b1d328c72361c749d9a4e789afb4bd9e7d0c555b
Diego Faria <diegoafbrito12@gmail.com>
2023-07-09 15:01:43 -0400

    agregar nuevos datos formulario pagos

--- pagos/urls.py
+++ pagos/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("registro/", formulario_pago, name="formulario_registro_pago"),
+    path("registro/<int:pk>/", formulario_pago, name="formulario_registro_pago"),
     path("cuenta/registro/", formulario_cuenta, name="formulario_registro_cuenta"),
 ]
