4909fd6fc0e59fceffc8920cc8041aee563a0513
Diego Faria <diegoafbrito12@gmail.com>
2023-08-08 15:09:17 -0400

    ya es instalable la pwa

--- tesis/settings.py
+++ tesis/settings.py
@@ -140,8 +140,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-PWA_APP_NAME = 'My App'
-PWA_APP_DESCRIPTION = "My app description"
+PWA_APP_NAME = 'Inmuebles Incaibo'
+PWA_APP_DESCRIPTION = "Aplicación web progresiva para la comercialización de inmuebles con soporte para personas con discapacidad visual mediante comandos de voz"
 PWA_APP_THEME_COLOR = '#0A0302'
 PWA_APP_BACKGROUND_COLOR = '#ffffff'
 PWA_APP_DISPLAY = 'standalone'
@@ -151,34 +151,34 @@ PWA_APP_START_URL = '/'
 PWA_APP_STATUS_BAR_COLOR = 'default'
 PWA_APP_ICONS = [
     {
-        'src': '/static/images/my_app_icon.png',
+        'src': '/static/img/logoCasa.png',
         'sizes': '160x160'
     }
 ]
 PWA_APP_ICONS_APPLE = [
     {
-        'src': '/static/images/my_apple_icon.png',
+        'src': '/static/img/logoCasa.png',
         'sizes': '160x160'
     }
 ]
 PWA_APP_SPLASH_SCREEN = [
     {
-        'src': '/static/images/icons/splash-640x1136.png',
+        'src': '/static/img/icons/logoCasa.png',
         'media': '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)'
     }
 ]
 PWA_APP_DIR = 'ltr'
-PWA_APP_LANG = 'en-US'
+PWA_APP_LANG = 'es-VE'
 PWA_APP_SHORTCUTS = [
     {
-        'name': 'Shortcut',
+        'name': 'Incaibo',
         'url': '/target',
-        'description': 'Shortcut to a page in my application'
+        'description': 'Acceso directo a Incaibo.'
     }
 ]
 PWA_APP_SCREENSHOTS = [
     {
-      'src': '/static/images/icons/splash-750x1334.png',
+      'src': '/static/img/logoCasa.png',
       'sizes': '750x1334',
       "type": "image/png"
     }
