0c25afe9a1b5f15e59a7217c62030eb7ee7fd204
Diego Faria <diegoafbrito12@gmail.com>
2023-08-09 20:07:19 -0400

    añadir validaciones al registro

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("register", register_user, name="registro")
+    path("register", register_user, name="registro"),
+    path("comprobar/correo/", comprobacion_correo, name="comprobar_correo"),
+    path("comprobar/cedula/", comprobacion_cedula, name="comprobar_cedula"),
 ]
